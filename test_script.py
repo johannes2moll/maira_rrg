6fcc7baaca3b5b2a09444ca05986fb443abb3091
Johannes Moll <johannes.moll@tum.de>
2024-12-16 10:19:28 -0800

    .

--- test_script.py
+++ test_script.py
@@ -1,17 +1,27 @@
 from transformers import AutoModelForCausalLM, AutoProcessor
 from pathlib import Path
 import torch
+from peft import PeftModel
+import torch
+import requests
+from PIL import Image
+from processing_maira2 import Maira2Processor
 
-model = AutoModelForCausalLM.from_pretrained("microsoft/maira-2", trust_remote_code=True)
-processor = AutoProcessor.from_pretrained("microsoft/maira-2", trust_remote_code=True)
 
-device = torch.device("cuda")
+
+base_model_name = "microsoft/maira-2"
+adapter_model_name = "StanfordAIMI/maira2-srrg-findings2"
+
+model = AutoModelForCausalLM.from_pretrained(base_model_name, trust_remote_code=True)
+processor = Maira2Processor.from_pretrained(base_model_name, trust_remote_code=True)
+
+model = PeftModel.from_pretrained(model, adapter_model_name)
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model = model.eval()
 model = model.to(device)
 
 
-import requests
-from PIL import Image
 
 def get_sample_data() -> dict[str, Image.Image | str]:
     """
@@ -34,7 +44,7 @@ def get_sample_data() -> dict[str, Image.Image | str]:
         "indication": "Dyspnea.",
         "comparison": "None.",
         "technique": "PA and lateral views of the chest.",
-        "phrase": "Pleural effusion."  # For the phrase grounding example. This patient has pleural effusion.
+        "phrase": "Pleural effusion." 
     }
     return sample_data
 
@@ -43,33 +53,26 @@ sample_data = get_sample_data()
 processed_inputs = processor.format_and_preprocess_reporting_input(
     current_frontal=sample_data["frontal"],
     current_lateral=sample_data["lateral"],
-    prior_frontal=None,  # Our example has no prior
+    prior_frontal=None, 
     indication=sample_data["indication"],
     technique=sample_data["technique"],
     comparison=sample_data["comparison"],
-    prior_report=None,  # Our example has no prior
+    prior_report=None, 
     return_tensors="pt",
-    get_grounding=False,  # For this example we generate a non-grounded report
+    get_grounding=False, 
 )
 
-print(f'sample images types: {type(sample_data["frontal"])}, {type(sample_data["lateral"])}')
-print(f'all info about images type and size and color: {sample_data["frontal"]}, {sample_data["lateral"]}, {sample_data["frontal"].size}, {sample_data["frontal"].mode}, {sample_data["lateral"].size}, {sample_data["lateral"].mode}')
-print("Processed inputs:", processed_inputs)
-print("Processed inputs keys:", processed_inputs.keys())
-print(f'decoded: {processor.decode(processed_inputs["input_ids"][0])}')
-print(f'pixel values info and type: {processed_inputs["pixel_values"].shape}, {processed_inputs["pixel_values"].dtype}')
-print(f'pixel values: {processed_inputs["pixel_values"]}')
 
 processed_inputs = processed_inputs.to(device)
 with torch.no_grad():
     output_decoding = model.generate(
         **processed_inputs,
-        max_new_tokens=300,  # Set to 450 for grounded reporting
+        max_new_tokens=300,
         use_cache=True,
     )
 prompt_length = processed_inputs["input_ids"].shape[-1]
 decoded_text = processor.decode(output_decoding[0][prompt_length:], skip_special_tokens=True)
-decoded_text = decoded_text.lstrip()  # Findings generation completions have a single leading space
+decoded_text = decoded_text.lstrip() 
 prediction = processor.convert_output_to_plaintext_or_grounded_sequence(decoded_text)
 print("Parsed prediction:", prediction)
 
